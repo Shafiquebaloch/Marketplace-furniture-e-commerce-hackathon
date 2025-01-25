d78e99424267977efb8f37fce25a9df1689efc77
Shafiquebaloch <muhammadraheem4739733@gmail.com>
2025-01-25 21:11:44 +0500

    update

--- furniro/next.config.mjs
+++ furniro/next.config.mjs
@@ -1,6 +1,11 @@
-export default{
+export default {
   images: {
     domains: ['cdn.sanity.io'], // Allow Sanity images
   },
   reactStrictMode: true, // Optional, recommended for production apps
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    NEXT_PUBLIC_SANITY_TOKEN: process.env.NEXT_PUBLIC_SANITY_TOKEN, // If you're using a token
+  },
 };
