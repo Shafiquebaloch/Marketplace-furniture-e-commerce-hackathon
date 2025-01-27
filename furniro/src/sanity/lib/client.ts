3c198cc583ee7ac273aad1e905ba584ef1c9d31e
Shafiquebaloch <muhammadraheem4739733@gmail.com>
2025-01-27 17:56:05 +0500

    updated code

--- furniro/src/sanity/lib/client.ts
+++ furniro/src/sanity/lib/client.ts
@@ -1,10 +1,14 @@
-import { createClient } from 'next-sanity'
+// src/sanity/lib/client.ts
+import { createClient } from 'next-sanity';
+import { apiVersion, dataset, projectId } from '../env';
 
-import { apiVersion, dataset, projectId } from '../env'
+const sanityToken = process.env.SANITY_TOKEN; // If you're using the token on the server side
 
+// Export the Sanity client with or without the token based on your need
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
-})
+  useCdn: true, // Set to false if you're using the token for server-side requests
+  token: sanityToken, // Only use token for server-side requests
+});
